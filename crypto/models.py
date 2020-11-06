20ec7b3b69272847eb230d7f71276567750c04da
gemma gonputh <gemmage3@hotmail.co.uk>
2020-11-06 14:05:34 +0000

    Adjusted model and migrate

--- crypto/models.py
+++ crypto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Crypto(models.Model):
+  name = models.CharField(max_length=150)
+  symbol = models.CharField(max_length=5)
+  price = models.DecimalField(max_digits=19, decimal_places=10)
+  market_cap = models.DecimalField(max_digits=19, decimal_places=2)
