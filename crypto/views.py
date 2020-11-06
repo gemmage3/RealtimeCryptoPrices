40d124178055f52c69a99593570665f5bf9d9462
gemma gonputh <gemmage3@hotmail.co.uk>
2020-11-06 14:35:55 +0000

    update view

--- crypto/views.py
+++ crypto/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
+from .models import Crypto
+
 
 def index(request):
-    return HttpResponse("Hello, world")
+    crypto_list = Crypto.objects.order.all
+    output = ', '.join([q.name for q in crypto_list])
+    return HttpResponse(output)
